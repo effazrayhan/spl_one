44fbeb76bd8df836a6842df6986f5a6375f69d6b
effazrayhan <143859628+effazrayhan@users.noreply.github.com>
2025-01-21 12:36:23 +0600

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,41 +1,138 @@
-#include <iostream>
-#include <cstring>
-#include <thread>
+#include <bits/stdc++.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <string.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <thread>
+#include <signal.h>
+#include <mutex>
+#define MAX_LEN 200
+#define NUM_COLORS 6
 
-void receiveMessages(int socket) {
-    char buffer[1024];
-    while (true) {
-        int bytesReceived = recv(socket, buffer, 1024, 0);
-        if (bytesReceived <= 0) break;
-        std::cout << "Server: " << std::string(buffer, bytesReceived) << std::endl;
-    }
+using namespace std;
+
+bool exit_flag=false;
+thread t_send, t_recv;
+int client_socket;
+string def_col="\033[0m";
+string colors[]={"\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m", "\033[36m"};
+
+void catch_ctrl_c(int signal);
+string color(int code);
+int eraseText(int cnt);
+void send_message(int client_socket);
+void recv_message(int client_socket);
+
+int main()
+{
+	if((client_socket=socket(AF_INET,SOCK_STREAM,0))==-1)
+	{
+		perror("socket: ");
+		exit(-1);
+	}
+
+	struct sockaddr_in client;
+	client.sin_family=AF_INET;
+	client.sin_port=htons(10000); 
+	client.sin_addr.s_addr=INADDR_ANY;
+	bzero(&client.sin_zero,0);
+
+	if((connect(client_socket,(struct sockaddr *)&client,sizeof(struct sockaddr_in)))==-1)
+	{
+		perror("connect: ");
+		exit(-1);
+	}
+	signal(SIGINT, catch_ctrl_c);
+	char name[MAX_LEN];
+	cout<<"Enter your name : ";
+	cin.getline(name,MAX_LEN);
+	send(client_socket,name,sizeof(name),0);
+
+	cout<<colors[NUM_COLORS-1]<<"\n\t  ====== Welcome to the chat-room ======   "<<endl<<def_col;
+
+	thread t1(send_message, client_socket);
+	thread t2(recv_message, client_socket);
+
+	t_send=move(t1);
+	t_recv=move(t2);
+
+	if(t_send.joinable())
+		t_send.join();
+	if(t_recv.joinable())
+		t_recv.join();
+			
+	return 0;
 }
 
-int main() {
-    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    sockaddr_in serverAddr{};
+// Handler for "Ctrl + C"
+void catch_ctrl_c(int signal) 
+{
+	char str[MAX_LEN]="#exit";
+	send(client_socket,str,sizeof(str),0);
+	exit_flag=true;
+	t_send.detach();
+	t_recv.detach();
+	close(client_socket);
+	exit(signal);
+}
 
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(8080); // Same port as server
-    inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr); // Server IP (Localhost for LAN)
+string color(int code)
+{
+	return colors[code%NUM_COLORS];
+}
 
-    if (connect(clientSocket, (sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
-        std::cerr << "Connection failed.\n";
-        return -1;
-    }
+// Erase text from terminal
+int eraseText(int cnt)
+{
+	char back_space=8;
+	for(int i=0; i<cnt; i++)
+	{
+		cout<<back_space;
+	}	
+}
 
-    std::thread(receiveMessages, clientSocket).detach();
+// Send message to everyone
+void send_message(int client_socket)
+{
+	while(1)
+	{
+		cout<<colors[1]<<"You : "<<def_col;
+		char str[MAX_LEN];
+		cin.getline(str,MAX_LEN);
+		send(client_socket,str,sizeof(str),0);
+		if(strcmp(str,"#exit")==0)
+		{
+			exit_flag=true;
+			t_recv.detach();	
+			close(client_socket);
+			return;
+		}	
+	}		
+}
 
-    char buffer[1024];
-    while (true) {
-        std::cin.getline(buffer, 1024);
-        if (std::string(buffer) == "exit") break;
-
-        send(clientSocket, buffer, strlen(buffer), 0);
-    }
-
-    close(clientSocket);
-    return 0;
+// Receive message
+void recv_message(int client_socket)
+{
+	while(1)
+	{
+		if(exit_flag)
+			return;
+		char name[MAX_LEN], str[MAX_LEN];
+		int color_code;
+		int bytes_received=recv(client_socket,name,sizeof(name),0);
+		if(bytes_received<=0)
+			continue;
+		recv(client_socket,&color_code,sizeof(color_code),0);
+		recv(client_socket,str,sizeof(str),0);
+		eraseText(6);
+		if(strcmp(name,"#NULL")!=0)
+			cout<<color(color_code)<<name<<" : "<<def_col<<str<<endl;
+		else
+			cout<<color(color_code)<<str<<endl;
+		cout<<colors[1]<<"You : "<<def_col;
+		fflush(stdout);
+	}	
 }
