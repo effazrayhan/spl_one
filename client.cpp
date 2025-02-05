4589dd3c75bd2c692741a933cda51f4bd823106a
effazrayhan <143859628+effazrayhan@users.noreply.github.com>
2025-02-05 12:20:24 +0600

    Updated Multiple client handling ability over same device

--- client.cpp
+++ client.cpp
@@ -1,138 +1,51 @@
-#include <bits/stdc++.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <errno.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <unistd.h>
+#include <iostream>
 #include <thread>
-#include <signal.h>
-#include <mutex>
-#define MAX_LEN 200
-#define NUM_COLORS 6
+#include <string>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <string.h>
 
+#define PORT 8080
+#define SERVER_IP "127.0.0.1"
 using namespace std;
-
-bool exit_flag=false;
-thread t_send, t_recv;
-int client_socket;
-string def_col="\033[0m";
-string colors[]={"\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m", "\033[36m"};
-
-void catch_ctrl_c(int signal);
-string color(int code);
-int eraseText(int cnt);
-void send_message(int client_socket);
-void recv_message(int client_socket);
-
-int main()
-{
-	if((client_socket=socket(AF_INET,SOCK_STREAM,0))==-1)
-	{
-		perror("socket: ");
-		exit(-1);
-	}
-
-	struct sockaddr_in client;
-	client.sin_family=AF_INET;
-	client.sin_port=htons(10000); 
-	client.sin_addr.s_addr=INADDR_ANY;
-	bzero(&client.sin_zero,0);
-
-	if((connect(client_socket,(struct sockaddr *)&client,sizeof(struct sockaddr_in)))==-1)
-	{
-		perror("connect: ");
-		exit(-1);
-	}
-	signal(SIGINT, catch_ctrl_c);
-	char name[MAX_LEN];
-	cout<<"Enter your name : ";
-	cin.getline(name,MAX_LEN);
-	send(client_socket,name,sizeof(name),0);
-
-	cout<<colors[NUM_COLORS-1]<<"\n\t  ====== Welcome to the chat-room ======   "<<endl<<def_col;
-
-	thread t1(send_message, client_socket);
-	thread t2(recv_message, client_socket);
-
-	t_send=move(t1);
-	t_recv=move(t2);
-
-	if(t_send.joinable())
-		t_send.join();
-	if(t_recv.joinable())
-		t_recv.join();
-			
-	return 0;
+void receive_messages(int socket) {
+    char buffer[1024];
+    while (true) {
+        memset(buffer, 0, 1024);
+        int bytes_received = recv(socket, buffer, 1024, 0);
+        if (bytes_received <= 0) {
+            cout << "Disconnected from server." << endl;
+            break;
+        }
+        cout << buffer << endl;
+    }
 }
 
-// Handler for "Ctrl + C"
-void catch_ctrl_c(int signal) 
-{
-	char str[MAX_LEN]="#exit";
-	send(client_socket,str,sizeof(str),0);
-	exit_flag=true;
-	t_send.detach();
-	t_recv.detach();
-	close(client_socket);
-	exit(signal);
-}
+int main() {
+    int client_socket;
+    sockaddr_in server_addr;
 
-string color(int code)
-{
-	return colors[code%NUM_COLORS];
-}
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr);
 
-// Erase text from terminal
-int eraseText(int cnt)
-{
-	char back_space=8;
-	for(int i=0; i<cnt; i++)
-	{
-		cout<<back_space;
-	}	
-}
+    if (connect(client_socket, (sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        cerr << "Connection to server failed!" << endl;
+        return 1;
+    }
 
-// Send message to everyone
-void send_message(int client_socket)
-{
-	while(1)
-	{
-		cout<<colors[1]<<"You : "<<def_col;
-		char str[MAX_LEN];
-		cin.getline(str,MAX_LEN);
-		send(client_socket,str,sizeof(str),0);
-		if(strcmp(str,"#exit")==0)
-		{
-			exit_flag=true;
-			t_recv.detach();	
-			close(client_socket);
-			return;
-		}	
-	}		
-}
+    cout << "Connected to server. Start chatting!" << endl;
+    thread receive_thread(receive_messages, client_socket);
+    
+    string message;
+    while (true) {
+        getline(cin, message);
+        if (message == "exit") break;
+        send(client_socket, message.c_str(), message.length(), 0);
+    }
 
-// Receive message
-void recv_message(int client_socket)
-{
-	while(1)
-	{
-		if(exit_flag)
-			return;
-		char name[MAX_LEN], str[MAX_LEN];
-		int color_code;
-		int bytes_received=recv(client_socket,name,sizeof(name),0);
-		if(bytes_received<=0)
-			continue;
-		recv(client_socket,&color_code,sizeof(color_code),0);
-		recv(client_socket,str,sizeof(str),0);
-		eraseText(6);
-		if(strcmp(name,"#NULL")!=0)
-			cout<<color(color_code)<<name<<" : "<<def_col<<str<<endl;
-		else
-			cout<<color(color_code)<<str<<endl;
-		cout<<colors[1]<<"You : "<<def_col;
-		fflush(stdout);
-	}	
+    close(client_socket);
+    receive_thread.join();
+    return 0;
 }
